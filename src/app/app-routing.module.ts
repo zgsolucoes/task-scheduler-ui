507af3c79de8717d8995a25fc8e6af6839c28607
Luiz Henrique <luizhenrique@zgsolucoes.com.br>
2020-09-25 12:28:13 -0300

    #6; MSG: Criação da página de login com um mecanismo de autenticação básico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroUsuarioComponent } from './components/usuarios/cadastro-usuario/cadastro-usuario.component';
+import { LoginComponent } from './components/login/login/login.component';
 
 const routes: Routes = [
-    { path: 'cadastro', component: CadastroUsuarioComponent }
+    { path: 'cadastro', component: CadastroUsuarioComponent },
+    { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { UsuarioService } from './services/usuario.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector   : 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls  : ['./app.component.css']
 })
 export class AppComponent {
-  title = 'task-scheduler-ui';
+    title = 'task-scheduler-ui';
+
+    constructor(private usuarioService: UsuarioService) {
+    }
+
+    nomeUsuarioLogado(): string {
+        return this.usuarioService.getNomeUsuarioLogado();
+    }
+
+    existeUsuarioAutenticado(): boolean {
+        return this.usuarioService.existeUsuarioAutenticado();
+    }
+
+    realizarLogout(): void {
+        this.usuarioService.realizarLogout();
+    }
 }
